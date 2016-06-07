44f1a52198e1718fceea669ae9065ab66210905f
eydelrivero <eydel.rivero@gmail.com>
2016-06-07 17:15:02 +0200

    test

--- Formula/apache-spark.rb
+++ Formula/apache-spark.rb
@@ -16,6 +16,11 @@ class ApacheSpark < Formula
     libexec.install Dir["*"]
     bin.write_exec_script Dir["#{libexec}/bin/*"]
   end
+  
+  devel do
+    url "http://apache.rediris.es/spark/spark-2.0.0-preview/spark-2.0.0-preview.tgz"
+    sha256 "iQEcBAABAgAGBQJXO8P7AAoJEHxsEF/8jtCJk20H/RW/OKh3tAitQvyfkiTRbNEYMpuGEgPxhkNZvIADmPt/e75G6whnuRWtFQxkAUGngyZ/TKrD46IchV6ys143o8ob3OtprgyqMKkziHw4FlkeH16Ku0Y6UqLyB0jgJzrVk1U755NN1FfDKNsWYPr0D5Hhu1hU9956DUi8+AlmwTJ7hzRDBItQunA2iI/au3AtkZKFa15GKgYm1Wptbnx97rN5Kz9E1rpLIrAYqREC+lPRbBsWYWEJXupR892/2SD0mA8E54WhAh/gy1Ky48g7taOcH3t7ZH3v+YMsw/zP+Kuk4lmYFpeU1iXbgKVyHUZYQisa9PWPFUMwyV1sK5CFJX4==aO08"
+  end
 
   test do
     system "#{bin}/spark-shell <<<'sc.parallelize(1 to 1000).count()'"
